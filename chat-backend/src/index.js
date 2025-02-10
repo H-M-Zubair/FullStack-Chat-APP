f6212c03dde6f3605c6307174f9f93a957cdf989
Zubair <h.m.zubairrafiq@gmail.com>
2025-02-10 17:41:26 +0500

    Deployments changes

--- chat-backend/src/index.js
+++ chat-backend/src/index.js
@@ -6,12 +6,14 @@ import cookieParser from "cookie-parser";
 import messageRouter from "./routes/message.routes.js";
 import cors from "cors";
 import { server, app } from "./lib/socket.js";
+import path from "path";
 dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json({ limit: "5mb" })); //to allow parsing limit 5mb by default it is 100KB
 
 const port = 5001;
+const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -21,6 +23,13 @@ app.use(
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../chat-frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../chat-frontend/dist/index.html"));
+  });
+}
 server.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
   dbConnection();
